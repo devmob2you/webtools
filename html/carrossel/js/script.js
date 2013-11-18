3998677062cb06281098121237281deb5efc6897
programacao02 <programacao02@Programacao002.betonico.local>
2013-11-18 08:47:29 -0200

    Slider Carrossel sem plugin

---
+++ html/carrossel/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".carosel").jCarouselLite({
+      btnNext: ".next",
+      btnPrev: ".prev",
+      visible: 1,
+      speed: 1500
+        /*Quantidade de fotos por vez*/
+      /*auto: 2000, /*Tempo de espera para a proxima transicao
+      speed: 1000 /*Velocidade da transicao*/
+    });
+ });
+
